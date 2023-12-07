81e9921257864c737b708544643492fa63590f9d
KELVIN MURITHI <kelvinmurithi@live.com>
2023-12-08 01:51:35 +0300

    all tests passing, completed assignment

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,15 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+    def get_element_by_id(self, id):
+        return self._get_element_by_id(self.root, id)
+
+    def _get_element_by_id(self, node, id):
+        if node['id'] == id:
+            return node
+        for child in node['children']:
+            result = self._get_element_by_id(child, id)
+            if result is not None:
+                return result
+        return None
